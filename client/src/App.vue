602405f2e063f840747bb868b890e4dceb80a381
Raymundo Vasquez Ruiz <raymundo.vr@protonmail.com>
2019-02-14 22:04:07 +0100

    adding balls

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,35 +4,45 @@
     <div v-for="(link, index) in links" :key="index">
       <ResultLink v-bind:name="link" />
     </div>    
-  </div>
+    <ResultSVG :source="source" v-bind:links="links" />
+  </div>  
 </template>
 
 <script>
 import Search from './components/Search';
 import ResultLink from './components/ResultLink';
+import ResultSVG from './components/ResultSVG';
 
 export default {
   name: 'app',  
   components: {
     Search,
-    ResultLink
+    ResultLink,
+    ResultSVG
   },
   data: function () {
     return {
-      links: []
+      links: [],
+      vertex: {},
+      source: '',
     }
   },
   methods: {
-    inspectLink: async function(url) {
-      console.log('listening...');
+    inspectLink: async function(url) {      
       const queryUrl = `http://localhost:3000?u=${url}`;
       try {
         let response = await fetch(queryUrl);
         let result = await response.json();
         this.links = result.matches; 
+        this.source = url;
+        this.buildGraph(url, this.links);
       } catch (err) {
         console.error(err);
       }      
+    },
+    buildGraph: function(source, links) {
+      this.vertex = { source: links };
+      console.log(this.vertex);
     }
   }
 }
